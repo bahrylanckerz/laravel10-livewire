3185333147a7f819a4a584423ddaaea9408d49f8
Bahry Lanckerz <bahrylanckerz@gmail.com>
2023-12-21 14:27:42 +0700

    add services

---
+++ app/Filament/Resources/ServiceResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Service;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\RichEditor;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use App\Filament\Resources\ServiceResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ServiceResource\RelationManagers;
+use Filament\Tables\Actions\DeleteAction;
+
+class ServiceResource extends Resource
+{
+    protected static ?string $model = Service::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')->label('Title')->required(),
+                TextInput::make('icon_class')->label('Icon')->required(),
+                TextInput::make('short_desc')->label('Short Description')->columnSpan(2)->required(),
+                RichEditor::make('long_desc')->label('Description')->columnSpan(2)->required(),
+                Select::make('status')
+                    ->options([
+                        1 => 'Active',
+                        0 => 'Suspend',
+                    ])
+                    ->hiddenOn('create')
+                    ->visibleOn('edit'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->label('Title'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'  => Pages\ListServices::route('/'),
+            'create' => Pages\CreateService::route('/create'),
+            'edit'   => Pages\EditService::route('/{record}/edit'),
+        ];
+    }
+}
